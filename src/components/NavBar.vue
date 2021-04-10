23fe063e178024afe6d4323b53dd126ad1984123
Marco Taubert <mtaubert@neuro-net.eu>
2021-04-10 15:56:14 +0200

    - Add Navbar - Add AppBar - Add ControlContainer

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="ma-12 pa-12">
+    <v-card>
+      <v-navigation-drawer
+        permanent
+        expand-on-hover
+        app
+      >
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="../assets/logo.png"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list nav dense>
+          <v-list-item link="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item link="commands">
+            <v-list-item-icon>
+              <v-icon>mdi-message-bulleted</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Befehle</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+
+    data () {
+      return { }
+    },
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col cols="12" sm="12" md="1" class="pa-2 d-flex justify-center">
+          <v-btn color="success" elevation="2" fab>
+            <v-icon>mdi-check</v-icon>
+          </v-btn>
+          <h5 class="mt-5 ml-2">VERBUNDEN</h5>
+      </v-col>
+      <v-col cols="12" md="8" class="pa-2 d-flex justify-center align-center">
+          <h3>------------------------------------------------Statistik------------------------------------------------</h3>
+      </v-col>
+      <v-col cols="12" md="1" class="pa-2 d-flex justify-center align-center">
+        <v-btn
+          block
+          color="success"
+          elevation="2"
+          large
+        >Start</v-btn>
+      </v-col>
+      <v-col cols="12" md="1" class="pa-2 d-flex justify-center align-center">
+        <v-btn
+          block
+          color="warning"
+          elevation="2"
+          large
+        >Restart</v-btn>
+      </v-col>
+      <v-col cols="12" md="1" class="pa-2 d-flex justify-center align-center">
+        <v-btn
+          block
+          color="red"
+          elevation="2"
+          large
+        >Stop</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
